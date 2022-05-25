10844adc0cb403d5cd482c281db493990ca5345a
igor ricci <assis.ricci@gmail.com>
2022-05-25 12:19:59 -0300

    seeded 5 restaurants

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+restaurant = Restaurant.new(name: "Epicure", category: "french", address: "Salvador")
+restaurant.save
+restaurant = Restaurant.new(name: "Origem", category: "belgian", address: "Salvador")
+restaurant.save
+restaurant = Restaurant.new(name: "Sal & Brasa", category: "chinese", address: "Salvador")
+restaurant.save
+restaurant = Restaurant.new(name: "PastaFast", category: "italian", address: "Salvador")
+restaurant.save
+restaurant = Restaurant.new(name: "Oliva", category: "japanese", address: "Salvador")
+restaurant.save
