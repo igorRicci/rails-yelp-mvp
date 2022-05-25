274b0112ff112db7538bef7740bdc45c4bf910e1
igor ricci <assis.ricci@gmail.com>
2022-05-25 15:03:16 -0300

    removed new_review route an embbeded it to restaurant/show

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -15,7 +15,7 @@ class RestaurantsController < ApplicationController
   def create
     @restaurant = Restaurant.new(restaurant_params)
     if @restaurant.save
-      redirect_to restaurant_path(@restaurant)
+      redirect_to restaurant_path(@restaurant), notice: 'Restaurant was successfully created.'
     else
       render :new
     end
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -9,9 +9,11 @@ class ReviewsController < ApplicationController
     @review = Review.new(review_params)
     @review.restaurant = @restaurant
 
-    @review.save
-
-    redirect_to restaurant_path(@restaurant)
+    if @review.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render 'restaurants/show'
+    end
   end
 
   private
