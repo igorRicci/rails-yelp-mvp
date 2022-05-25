a1e0143da8795b9a833f7010e7814b49eb0199b2
igor ricci <assis.ricci@gmail.com>
2022-05-25 14:29:57 -0300

    added rest and review controllers. added restaurants_controller methods

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,6 +2,6 @@ class Restaurant < ApplicationRecord
   has_many :reviews, dependent: :destroy
 
   validates :name, :address, :category, presence: true
-  validates :category, inclusion: { in: ["chinese", "italian", "japanese", "french", "belgian"], message: "%{value} is not a valid category" }
+  validates :category, inclusion: { in: ["chinese", "italian", "japanese", "french", "belgian", "brazilian", "junk food"], message: "'%{value}' is not valid" }
 
 end
